b62e1a45b1b8515ab4e8946219fbf14ba4be5669
Aibek-21 <aibek61@gmail.com>
2021-01-06 22:02:18 -0800

    all steps works

--- src/test/java/com/nextBaseCRM/aibek/Task.java
+++ src/test/java/com/nextBaseCRM/aibek/Task.java
@@ -8,40 +8,66 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Task {
     public static void main(String[] args) throws InterruptedException {
 
+        //setup chrome browser
         WebDriverManager.chromedriver().setup();
+
+        //opening chrome
         WebDriver driver = new ChromeDriver();
+
+        //opening www,nextbasecrm.com
         driver.get("https://www.nextbasecrm.com");
+
+        //maximizing window
         driver.manage().window().maximize();
         Thread.sleep(2000);
 
+        //locating on login button and clicking
         driver.findElement(By.xpath("//*[@id=\"menu-item-516\"]/a")).click();
         Thread.sleep(2000);
+
+        //locating on login button and typing user name
         driver.findElement(By.className("login-inp")).sendKeys("helpdesk31@cybertekschool.com");
+
+        //locating on password button and typing password
         driver.findElement(By.name("USER_PASSWORD")).sendKeys("UserUser");
         Thread.sleep(2000);
+
+        //locating on login button clicking
         driver.findElement(By.className("login-btn")).click();
 
-        driver.findElement(By.xpath("//*[@id=\"feed-add-post-form-tab-vote\"]/span")).click();
+        //location on vote again button and click
+        driver.findElement(By.xpath("//*[@id=\"vote-nSLjLH51\"]/form/div[2]/button[1]")).click();
         Thread.sleep(3000);
-        driver.findElement(By.className("template-bitrix24 no-paddings start-page bitrix24-default-theme")).sendKeys("Testing poll!");
+
+        //locating on answer radio button and clicking
+        driver.findElement(By.xpath("//*[@id=\"question57\"]/table/tbody/tr[1]/td[1]/div/span/label[1]/span")).click();
+        Thread.sleep(2000);
+
+        //locating on vote button and clicking
+        driver.findElement(By.xpath("//*[@id=\"vote-nSLjLH51\"]/form/div[2]/button[2]")).click();
+        Thread.sleep(2000);
+
+        //locating on vote again button and clicking
+        driver.findElement(By.xpath("//*[@id=\"vote-nSLjLH51\"]/form/div[2]/button[1]")).click();
         Thread.sleep(3000);
-        driver.findElement(By.name("UF_BLOG_POST_VOTE_n0_DATA[QUESTIONS][0][QUESTION]")).sendKeys("What is the capital of US?");
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"answer_0__0_\"]")).sendKeys("Washington");
-        Thread.sleep(2000);
-        driver.findElement(By.name("UF_BLOG_POST_VOTE_n0_DATA[QUESTIONS][0][ANSWERS][1][MESSAGE]")).sendKeys("Chicago");
-        Thread.sleep(2000);
-        driver.findElement(By.id("blog-submit-button-save")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.className("bx-vote-block-inp-substitute")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.className("ui-btn ui-btn-lg ui-btn-link")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.className("ui-btn ui-btn-lg ui-btn-link")).click();
 
+        //locating on answer radio button and clicking
+        driver.findElement(By.xpath("//*[@id=\"question57\"]/table/tbody/tr[2]/td[1]/div/span/label[1]/span")).click();
+        Thread.sleep(3000);
 
+        //locating on vote button and clicking
+        driver.findElement(By.xpath("//*[@id=\"vote-nSLjLH51\"]/form/div[2]/button[2]")).click();
+        Thread.sleep(3000);
 
+        //locating on profile and clicking
+        driver.findElement(By.xpath("//span[@class='user-name']")).click();
+        Thread.sleep(3000);
+
+        //locating on log out and clicking
+        driver.findElement(By.xpath("//span[.='Log out']")).click();
     }
 
 }
+
+
 
