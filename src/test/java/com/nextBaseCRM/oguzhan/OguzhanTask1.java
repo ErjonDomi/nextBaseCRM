f903854d6c112b04583219e190109c344523975c
Oguzhan1978 <74024518+Oguzhan1978@users.noreply.github.com>
2021-01-08 21:52:11 -0500

    iframe works need

--- src/test/java/com/nextBaseCRM/oguzhan/OguzhanTask1.java
+++ src/test/java/com/nextBaseCRM/oguzhan/OguzhanTask1.java
@@ -3,6 +3,7 @@ package com.nextBaseCRM.oguzhan;
 import com.nextBaseCRM.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class OguzhanTask1 {
 
@@ -18,9 +19,26 @@ public class OguzhanTask1 {
         driver.findElement(By.name("USER_PASSWORD")).sendKeys("UserUser");
         driver.findElement(By.className("login-btn")).click();
 
-        driver.findElement(By.xpath("//span[.='Message']")).click();
-        driver.findElement(By.xpath("//body[@contenteditable='true']")).sendKeys("Hi Everyone! How is everything going?");
+       driver.findElement(By.xpath("//div[@id='microoPostFormLHE_blogPostForm_inner']")).click();
+        WebElement frame=driver.findElement(By.xpath("//*[@id=\"bx-html-editor-iframe-cnt-idPostFormLHE_blogPostForm\"]/iframe"));
+        driver.switchTo().frame(frame);
+        driver.findElement(By.xpath("/html/body")).sendKeys("Hi Everyone! How is everything going?");
+        driver.switchTo().parentFrame();
 
+
+      /*  driver.findElement(By.xpath("//span[.='Message']")).click();
+
+        WebElement frame = driver.findElement(By.xpath("//iframe[@class='bx-editor-iframe']"));
+         driver.switchTo().frame(frame);
+                 frame.sendKeys("Hi Everyone! How is everything going?");
+
+       // driver.findElement(By.xpath("//span[.='Message']")).click();
+
+       // WebElement frame = driver.findElement(By.xpath("//div[@id='bx-html-editor-iframe-cnt-idPostFormLHE_blogPostForm']"));
+        //driver.switchTo().frame(frame);
+
+      //  driver.findElement(By.xpath("//body[@contenteditable='true']")).sendKeys("Hi Everyone! How is everything going?");
+*/
 
 
 
