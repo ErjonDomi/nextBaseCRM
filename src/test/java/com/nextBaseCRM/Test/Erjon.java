f091b3269555aaf38487c5ab4981ae9d99917cfa
Malik <aamiramallick7@gmail.com>
2021-01-28 12:13:39 -0500

    accepting it

--- src/test/java/com/nextBaseCRM/Test/Erjon.java
+++ src/test/java/com/nextBaseCRM/Test/Erjon.java
@@ -3,6 +3,7 @@ package com.nextBaseCRM.Test;
 public class Erjon {
     public static void main(String[] args) {
         System.out.println("works all good");
+        System.out.println("ok");
 
     }
 }
